be646e8187b739198da62e2931cf3a0bff8e33a1
Kevin <kevin.v.zhang@td.com>
2017-05-26 22:38:05 -0400

    pushed broken camera code

--- Enghack_Receipt_Scanner/app/src/main/java/com/example/android/enghack_receipt_scanner/MainActivity.java
+++ Enghack_Receipt_Scanner/app/src/main/java/com/example/android/enghack_receipt_scanner/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.enghack_receipt_scanner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,10 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+
 public class MainActivity extends AppCompatActivity {
+    static private int RC_BARCODE_CAPTURE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Camera should be opened!", Toast.LENGTH_SHORT).show();
+                switchToCamera();
             }
         });
     }
@@ -31,5 +35,20 @@ public class MainActivity extends AppCompatActivity {
 
     public Typeface getFATypeface(Context context) {
         return Typeface.createFromAsset(context.getAssets(), "fonts/fontawesome-webfont.ttf");
+    }
+
+    public void switchToCamera() {
+        Intent intent = new Intent(this, OcrCaptureActivity.class);
+        startActivityForResult(intent, RC_BARCODE_CAPTURE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+
+                }
+            }
+        }
     }
 }
